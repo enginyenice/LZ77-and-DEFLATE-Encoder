b027f202cacb16abbb626b8092f2a6da38cc153d
Engin Yenice <enginyenice2626@gmail.com>
2020-05-09 18:01:27 +0300

    LZ77 Add

--- main.c
+++ main.c
@@ -1,57 +1,141 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#define dosyaOkumaBoyutu 1000000
-char a[dosyaOkumaBoyutu];
+#include <string.h>
+/** Sabitler */
+#define dosyaOkumaBoyutu 10000000
+#define true 1
+#define false 0
+
+/** Degiskenler */
+char text[dosyaOkumaBoyutu];
+
+/** Fonksiyonlar */
 void LZ77();
-void DEFLATE();
 void ReadFile();
+
+/*
+
+searchBuffer  -> 8
+forwordBuffer -> 6
+
+*/
+
+
+
+void encode(int offset,int lenght,char character,FILE *dosya)
+{
+    fprintf(dosya,"%c,%c,%c\n",(unsigned char)offset,(unsigned char)lenght,(unsigned char)character);
+}
+
 
 void LZ77()
 {
-    printf("%s", a);
 
-}
-void DEFLATE()
-{
-    printf("\nDEFLATE\n");
-}
-void ReadFile()
-{
-    FILE *dosya;
-    if ((dosya = fopen("metin.txt", "r")) == NULL)
+    FILE *encodeFile;
+    encodeFile = fopen("metin.lz77","ab+");
+
+    /**
+              8                 6               XXX
+        |**SEARCH**|------|**FORWARD**|-------|**DEVAMI**||
+    */
+    int pointer = 0;
+    while(pointer < strlen(text))
     {
-        printf("Dosya bulunamadi...");
-        system("exit");
-    }
-    else
-    {
-        for (int i = 0; i < dosyaOkumaBoyutu; i++)
+
+        int i= pointer -1;
+        if(i == -1)
         {
-            fscanf(dosya, "%c", &a[i]);
+            //printf("<0,0,%c>\n",text[pointer]);
+            encode(0,0,text[pointer],encodeFile);
+            pointer++;
+        }
+        else
+        {
+            int harfVarMi = false;
+            while (i >= 0)
+            {
+
+                if(text[i] == text[pointer])
+                {
+                    harfVarMi = true;
+                    break;
+                }
+                i--;
+            }
+            if(harfVarMi == false)
+            {
+                //printf("<0,0,%c>\n",text[pointer]);
+                encode(0,0,text[pointer],encodeFile);
+                pointer++;
+            }
+            else
+            {
+                int i = pointer-1,hafizaBenzerlik = 0,hangisindenItibaren = 0;
+
+                while(i >= 0)
+                {
+                    if(text[i] == text[pointer])
+                    {
+                        //printf("%d ",i); Benzerlik indisleri
+                        int benzerlik = 0;
+                        while(text[i+benzerlik] == text[pointer+benzerlik])
+                        {
+                            benzerlik++;
+                            //printf("asd");
+
+                        }
+                        if(benzerlik > hafizaBenzerlik)
+                        {
+                            hafizaBenzerlik = benzerlik;
+                            hangisindenItibaren = i;
+                        }
+                    }
+                    i--;
+                }
+
+
+                if(strlen(text)-1 < pointer+hafizaBenzerlik)
+                    {
+                       // printf("<%d,%d,%c>\n",(pointer-hangisindenItibaren),hafizaBenzerlik,text[pointer+hafizaBenzerlik-1]);
+                        encode((pointer-hangisindenItibaren),hafizaBenzerlik,text[pointer+hafizaBenzerlik-1],encodeFile);
+                    }
+                else
+                    {
+                      //  printf("<%d,%d,%c>\n",(pointer-hangisindenItibaren),hafizaBenzerlik,text[pointer+hafizaBenzerlik]);
+                        encode((pointer-hangisindenItibaren),hafizaBenzerlik,text[pointer+hafizaBenzerlik],encodeFile);
+                    }
+                pointer += hafizaBenzerlik +1;
+                //printf("Harf: %c | Nerede: %d | hangisindenItibaren %d | benzerlikUzunlugu: %d\n",text[pointer],pointer,hangisindenItibaren,hafizaBenzerlik);
+            }
         }
     }
-
+    fclose(encodeFile);
 }
+    void ReadFile()
+    {
+        FILE *dosya;
+        if((int)(dosya = fopen("metin.txt", "r")) == 0)
+            printf("Dosya bulunamadi");
+        else
+           {
+               char ch[10000];
+               while(!feof(dosya))
+               {
+                   fgets(ch,10000,dosya);
+                   strcat(text,ch);
+               }
 
-
-
-int main()
-
-{
-    setlocale(LC_ALL, "Turkish");
-    int menuInput = -1;
-    ReadFile();
-
-    printf("\n\t#MENU#\n");
-    printf("#1 -) LZ77 Algorýtmasi\n");
-    printf("#2 -) DEFLATE Algoritmasi\n");
-    printf("#0 -) CIKIS\n");
-    printf("#...DEF: 1");
-    LZ77();
-
-
-
-    return 0;
-
-}
+           }
+           fclose(dosya);
+    }
+    int main()
+    {
+        ReadFile();
+        //puts(text);
+        system("cls");
+        printf("Harf sayisi: %d Encode ediliyor...\n",strlen(text));
+        LZ77();
+        printf("Encode islemi tamamlandi...\n");
+        return 0;
+    }
