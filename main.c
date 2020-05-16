9fb1462aac81051657603ee68c8ca5fdd35d6ee4
Engin Yenice <enginyenice2626@gmail.com>
2020-05-17 00:03:24 +0300

    LZ77

--- main.c
+++ main.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
+#include <stdint.h>
 /** Sabitler */
-#define dosyaOkumaBoyutu 10000000
+#define dosyaOkumaBoyutu 50000000
 #define true 1
 #define false 0
 
@@ -13,49 +14,126 @@ char text[dosyaOkumaBoyutu];
 /** Fonksiyonlar */
 void LZ77();
 void ReadFile();
-
-/*
-
-searchBuffer  -> 8
-forwordBuffer -> 6
-
-*/
+void encode(int offset,int lenght,char character,FILE *encodeFile);
 
 
 
-void encode(int offset,int lenght,char character,FILE *dosya)
-{
-    fprintf(dosya,"%c,%c,%c\n",(unsigned char)offset,(unsigned char)lenght,(unsigned char)character);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 void LZ77()
 {
 
+    int searchBufferSize,forwardBufferSize;
+    ReadFile();
+    printf("\e[?25l");
+    printf("Search Buffer Lenght (Arama Tamponu Uzunlugu): ");
+    scanf("%d",&searchBufferSize);
+
+    printf("Forward Buffer Lenght (Ýleri Tamponu Uzunlugu): ");
+    scanf("%d",&forwardBufferSize);
+    system("cls");
+
+    printf("---LZ77 INFO---\nSearch Buffer : %d\n", searchBufferSize);
+    printf("Forward Buffer : %d\n", forwardBufferSize);
+    printf("In progress : \n");
     FILE *encodeFile;
-    encodeFile = fopen("metin.lz77","ab+");
+    encodeFile = fopen("metin.lz77","wb");
 
     /**
-              8                 6               XXX
+              256               256               XXX
         |**SEARCH**|------|**FORWARD**|-------|**DEVAMI**||
     */
-    int pointer = 0;
+    int pointer = 0,i = 0,harfVarMi = false,hafizaBenzerlik = 0,hangisindenItibaren = 0,benzerlik = 0;
     while(pointer < strlen(text))
     {
-
-        int i= pointer -1;
+        i= pointer -1;
         if(i == -1)
         {
-            //printf("<0,0,%c>\n",text[pointer]);
             encode(0,0,text[pointer],encodeFile);
             pointer++;
         }
         else
         {
-            int harfVarMi = false;
-            while (i >= 0)
+            harfVarMi = false;
+            while (i > pointer - searchBufferSize)
             {
-
                 if(text[i] == text[pointer])
                 {
                     harfVarMi = true;
@@ -65,26 +143,21 @@ void LZ77()
             }
             if(harfVarMi == false)
             {
-                //printf("<0,0,%c>\n",text[pointer]);
                 encode(0,0,text[pointer],encodeFile);
                 pointer++;
             }
             else
             {
-                int i = pointer-1,hafizaBenzerlik = 0,hangisindenItibaren = 0;
-
-                while(i >= 0)
+                hafizaBenzerlik = 0;
+                hangisindenItibaren = 0;
+                while(i > pointer - searchBufferSize)
                 {
                     if(text[i] == text[pointer])
                     {
-                        //printf("%d ",i); Benzerlik indisleri
-                        int benzerlik = 0;
-                        while(text[i+benzerlik] == text[pointer+benzerlik])
-                        {
+                        benzerlik = 0;
+                        while(text[i+benzerlik] == text[pointer+benzerlik] && benzerlik < forwardBufferSize)
                             benzerlik++;
-                            //printf("asd");
 
-                        }
                         if(benzerlik > hafizaBenzerlik)
                         {
                             hafizaBenzerlik = benzerlik;
@@ -94,48 +167,121 @@ void LZ77()
                     i--;
                 }
 
-
-                if(strlen(text)-1 < pointer+hafizaBenzerlik)
-                    {
-                       // printf("<%d,%d,%c>\n",(pointer-hangisindenItibaren),hafizaBenzerlik,text[pointer+hafizaBenzerlik-1]);
-                        encode((pointer-hangisindenItibaren),hafizaBenzerlik,text[pointer+hafizaBenzerlik-1],encodeFile);
-                    }
-                else
-                    {
-                      //  printf("<%d,%d,%c>\n",(pointer-hangisindenItibaren),hafizaBenzerlik,text[pointer+hafizaBenzerlik]);
-                        encode((pointer-hangisindenItibaren),hafizaBenzerlik,text[pointer+hafizaBenzerlik],encodeFile);
-                    }
+                encode((pointer-hangisindenItibaren),hafizaBenzerlik,text[pointer+hafizaBenzerlik],encodeFile);
                 pointer += hafizaBenzerlik +1;
-                //printf("Harf: %c | Nerede: %d | hangisindenItibaren %d | benzerlikUzunlugu: %d\n",text[pointer],pointer,hangisindenItibaren,hafizaBenzerlik);
             }
         }
+        /*
+        total 100
+        pointer x
+        */
+
+        if((pointer * 100  ) / strlen(text) % 5 == 0)
+            printf("\rDurum: %d%%",(pointer * 100  ) / strlen(text));
     }
     fclose(encodeFile);
-}
-    void ReadFile()
-    {
-        FILE *dosya;
-        if((int)(dosya = fopen("metin.txt", "r")) == 0)
-            printf("Dosya bulunamadi");
-        else
-           {
-               char ch[10000];
-               while(!feof(dosya))
-               {
-                   fgets(ch,10000,dosya);
-                   strcat(text,ch);
-               }
+    sikistirmaOrani();
 
-           }
-           fclose(dosya);
-    }
-    int main()
+}
+
+void sikistirmaOrani()
+{
+    int orjinalDosyaBoyutu,sikismisDosyaBoyutu;
+    FILE *orDosya = fopen("metin.txt", "rb");
+    fseek(orDosya,0,SEEK_END);
+    orjinalDosyaBoyutu = ftell(orDosya);
+    fclose(orDosya);
+    FILE *lzdosya = fopen("metin.lz77", "rb");
+    fseek(lzdosya,0,SEEK_END);
+    sikismisDosyaBoyutu = ftell(lzdosya);
+    fclose(lzdosya);
+    /* ORJ 100
+       SKT  ?
+       SKT * 100  / ORJ
+    */
+    printf("\n---------------------------------------\n");
+    printf("Dosya boyutu : %d byte\n",orjinalDosyaBoyutu);
+    printf("Sikistirilmis dosya boyutu: %d byte\n",sikismisDosyaBoyutu);
+    float sikistirilmisYuzde;
+    sikistirilmisYuzde = 100 - ((sikismisDosyaBoyutu * 100.0) / orjinalDosyaBoyutu);
+    printf("Sikistirma orani: [%.2f%%]\n",sikistirilmisYuzde);
+    printf("\n---------------------------------------\n");
+
+}
+
+void ReadFile()
+{
+    for(int i = 0; i<strlen(text);i++)
     {
-        ReadFile();
-        //puts(text);
-        system("cls");
-        printf("Harf sayisi: %d Encode ediliyor...\n",strlen(text));
-        LZ77();
-        printf("Encode islemi tamamlandi...\n");
-        return 0;
+        text[i] = "\0";
     }
+    FILE *dosya;
+    if((int)(dosya = fopen("metin.txt", "r")) == 0)
+        printf("Dosya bulunamadi");
+    else
+    {
+        char ch[10000];
+        while(!feof(dosya))
+        {
+            fgets(ch,10000,dosya);
+            strcat(text,ch);
+        }
+
+    }
+    fclose(dosya);
+}
+
+struct encoNode
+{
+    uint16_t offsetLen;
+    char harf;
+};
+void encode(int offset,int lenght,char character,FILE *encodeFile)
+{
+
+    //printf("<%d,%d,C(%c)>\n",offset,lenght,character);
+    offset = offset<<4;
+    offset += lenght;
+
+
+    struct encoNode temp;
+    temp.harf = character;
+    temp.offsetLen = offset;
+
+    //printf("Dosya Bicimi: \t<%d,C(%c)> \n",offset,character);
+    //fprintf(dosya,"%c%c",offset,(unsigned char)character);
+    fwrite(&temp,sizeof(temp),1,encodeFile);
+
+}
+int main()
+{
+    setlocale(LC_ALL, "Turkish");
+    system("cls");
+    int secim = 0;
+
+    do{
+        printf("\e[?25h");
+        printf("\n\n------------------\n-------MENU-------\n------------------\n");
+        printf("---- 1-) LZ77 ----\n");
+        printf("---- 2-) DEFLATE -\n");
+        printf("---- 9-) Cikis ---\n");
+        printf("------------------\n");
+        printf("Secim Yapiniz: ");
+        scanf("%d",&secim);
+        switch(secim)
+        {
+        case 1:
+            LZ77();
+            break;
+        case 2:
+            printf("---DEFLATE---");
+            break;
+        case 9:
+            return 0;
+            break;
+        }
+
+
+    }while(secim != 9);
+    return 0;
+}
